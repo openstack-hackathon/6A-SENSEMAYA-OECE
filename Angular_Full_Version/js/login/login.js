417e4bc0041e6f61a7e1fd932c95b1e9d1cb4e4d
alfredo <benitoalfredoreyes@gmail.com>
2016-09-10 09:43:13 -0500

    Login

--- Angular_Full_Version/js/login/login.js
+++ Angular_Full_Version/js/login/login.js
@@ -1,8 +1,6 @@
 function LonginCtrl($location, $scope) {
 
 	var login = this;
-	console.log("Hola");
-	console.log("Holaqwe2ew");
 
 	$scope.login = {
 		user : "us",
@@ -16,7 +14,36 @@ function LonginCtrl($location, $scope) {
 		console.log("click");
 		console.log($scope.user);
 		console.log($scope.password);
-		$location.path('/#');
+		/*request({
+	    method:'POST',
+	    body:{
+	      "password" : "12345",
+	      "user" : "medico-123123",
+	      "pass" : "qwerty"
+	    },
+	    uri: 'http://172.16.11.97:8989/',
+	    json: true
+	    }, (err, res, body) =>{
+	       console.log(body);	       
+	       if(body.successPassword === true){
+	          //
+
+	       }else{
+	          alert("Contraseña Incorrecta!", "Intentalo de nuevo...!", "error");
+	          
+	       }
+	       */
+	       var rol=$scope.user;
+	       console.log("El rol es: "+rol);
+	       if(rol=="paciente"){
+	       	$location.path('/dashboards/1');
+	       }
+	       else if(rol=="medico"){
+	       		$location.path('/doc/dash');
+	       }else{
+	       	$location.path('/pharm/dash');
+	       }
+		
 	}
 
 	 
