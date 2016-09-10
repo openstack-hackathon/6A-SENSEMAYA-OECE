6dc5910d3954238b995375324aa5d61faa1be88a
mottaman <Apple@MacBook-Pro-de-Apple-7.local>
2016-09-10 04:10:33 -0500

    cambios

--- NODE_SERVICE/routes/login.js
+++ NODE_SERVICE/routes/login.js
@@ -1,13 +1,22 @@
 var express = require('express'),
-    router = express.Router(),
-    ScreenModel = require('../model/Screen');
+    router = express.Router();
 
     router.get('/', function(req, res){
 
-      ScreenModel.find({},function(err, doc){
-        res.send({result:doc})
-      })
+      res.send({success:true})
 
-    });    
+    });
+
+    router.post('/', function(req, res){
+      var login = req.body;
+      console.log(req.body);
+
+      var splitUsr = login.user.split('-');
+
+      var result = {rol : splitUsr[0], userName : login.user};
+
+      res.send({success:true, result : result})
+
+    });
 
 module.exports = router;
