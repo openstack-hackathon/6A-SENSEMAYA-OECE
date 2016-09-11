384af4637b591fd4d236e55f14468be5cc5871c2
vampiro <vampiro@MacBook-Pro-de-MacBookPro.local>
2016-09-11 01:40:25 -0500

    merge recetas

--- Angular_Full_Version/js/login/login.js
+++ Angular_Full_Version/js/login/login.js
@@ -1,4 +1,4 @@
-function LonginCtrl($location, $scope, $http) {
+function LonginCtrl($location, $scope, $http, $rootScope) {
 
 	var login = this;
 
@@ -6,7 +6,7 @@ function LonginCtrl($location, $scope, $http) {
 		user : "us",
 		password : "pas"
 	};
-	
+
 	$scope.user = "";
 	$scope.password = "";
 
@@ -14,15 +14,16 @@ function LonginCtrl($location, $scope, $http) {
 		console.log("click");
 		console.log($scope.user);
 		console.log($scope.password);
-    
-    $http.post("http://172.16.11.97:3000/login/",{user:""+$scope.user, password:""+$scope.password})
-    	.success(function(data){       	 	
-    		var rol=data.result.rol;
+
+    $http.post("http://172.16.1.43:3000/login/",{user:""+$scope.user, password:""+$scope.password})
+    	.success(function(data){
+				$rootScope.userSession = data;
+    		var rol=data.rol;
 	       console.log("El rol es: "+rol);
 	       if(rol=="Paciente"){
 	       	$location.path('/dashboards/1');
 	       }
-	       else if(rol=="Medico"){
+	       else if(rol=="medico"){
 	       		$location.path('/doc/dash');
 	       }else{
 	       	$location.path('/pharm/dash');
@@ -32,10 +33,10 @@ function LonginCtrl($location, $scope, $http) {
 			swal("Contraseña Incorrecta!", "Intentalo de nuevo...!", "error");
 			$location.path('/#/login');
         })
-		
+
 	}
 
-	 
+
 }
 /**
  *
--- Angular_Full_Version/js/receta/receta.js
+++ Angular_Full_Version/js/receta/receta.js
@@ -1,11 +1,77 @@
 function RecetaCtrl($location, $scope, $http,$rootScope) {
-	console.log($rootScope.paciente);
+	console.log("$rootScope.paciente");
+  console.log($rootScope.paciente);
+  $scope.paciente = $rootScope.paciente.result;
+  $scope.idPaciente = $scope.paciente._id;
+  $scope.datPaciente = {
+    "nombrePaciente": $scope.paciente.nombre + " " +$scope.paciente.apellidoPaterno + " " +$scope.paciente.apellidoMaterno,
+    "peso":$scope.paciente.peso,
+    "statura":$scope.paciente.statura,
+    "presion":"",
+    "temperatura":"",
+    "subjetivo":"",
+    "objetivo":"",
+    "analisis":"",
+    "diagnostico":"",
+    "tratamiento":"",
+    "laboratorio":""
+  }
+  // $scope.nombrePaciente = $scope.paciente.nombre + " " +$scope.paciente.apellidoPaterno + " " +$scope.paciente.apellidoMaterno;
 
-	$scope.realizatx = function(){
+  $scope.checkboxModel = {
+         value1 : true,
+         value2 : 'YES'
+       };
+
+
+  console.log("$rootScope.session");
+  console.log($rootScope.userSession);
+  $scope.medico=$rootScope.userSession.result;
+  $scope.nombreMedico = $scope.medico.nombre + " " + $scope.medico.apellidoPaterno + " " + $scope.medico.apellidoMaterno;
+
+
+/*
+POST  http://127.16.1.43:3000/exp/notaMedica/57d437a7ab27a76ed76bb56e
+
+{
+ "medico" : "Luis perez",
+ "especialidad" : "Neurlogo",
+ "subjetivo" : "El paciente anuncia una falta de vision al caminar",
+ "objetivo" : "FC. 80/120 Temperatura 29 paciente lucido presenta un cuadro nervioso",
+ "analisis" : "Se encuentra hitoria clinica turbia",
+ "diagnostico" : "Paciente con crisis nerviosa convulsiva",
+ "tratamiento": "100 mg de paracetamol",
+ "laboratorio" : "0"
+}
+*/
+
+  $scope.guardaReceta =  function(){
+
+    $scope.datPaciente.medico = $scope.medico;
+    $scope.datPaciente.medico = "Neurlogo";
+    console.log("Guarda Receta");
+    console.log($scope.datPaciente);
+    $http({
+					  method : "POST",
+					  data: $scope.datPaciente,
+					  url : " http://172LOR.16.1.43:3000/exp/notaMedica/"+$scope.idPaciente
+				  }).success(function(resultado, status, headers, config){
+							console.log("Nota medica guardada");
+				  });
+
+
+
+  }
+
+  $scope.realizatx = function(){
 		console.log("realizartx...");
 		console.log($rootScope.paciente);
 		$location.path('/doc/receta');
 	}
+
+  $scope.example = {
+         value: new Date(2016, 10, 11)
+       };
 }
 /**
  *
